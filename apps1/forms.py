dac7cb3123dd5de952f1ccadb3d3e8f3a1340153
anandsuresh17 <anandsuresh1704@gmail.com>
2026-08-07 00:26:43 +0530

    registration model added

--- apps1/forms.py
+++ apps1/forms.py
@@ -1,6 +1,26 @@
-# from django import forms
-# from .models import Students
-# class Studentform(forms.ModelForm):
-#     class Meta:
-#         model=Students
-#         fields='__all__' 
+from django import forms
+from .models import Students
+class Studentform(forms.ModelForm):
+    class Meta:
+        model=Students
+        fields='__all__' 
+
+    hobbies = forms.MultipleChoiceField(
+    choices=[
+        ("reading", "Reading"),
+        ("sports", "Sports"),
+        ("music", "Music"),
+        ("dance", "Dance"),
+        ("painting", "Painting"),
+        ("coding", "Coding"),
+        ("photography", "Photography"),
+        ("public_speaking", "Public Speaking"),
+        ("volunteering", "Volunteering"),
+        ("gaming", "Gaming"),
+    ],
+    widget=forms.CheckboxSelectMultiple,
+    required=False
+)
+    class Meta:
+           model=Students
+           fields='__all__'
