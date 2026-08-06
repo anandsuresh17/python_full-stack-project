f214e44d5ed203f6da32a56210c95859cf4e5be9
anandsuresh17 <anandsuresh1704@gmail.com>
2026-08-07 00:26:43 +0530

    registration model added

--- apps1/models.py
+++ apps1/models.py
@@ -1,25 +1,25 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Hobby(models.Model):
-#     name = models.CharField(max_length=100)
+# Create your models here.
+class Hobby(models.Model):
+    name = models.CharField(max_length=100)
 
-# class Students(models.Model):
+class Students(models.Model):
 
-# # columns-
+# columns-
 
-#     fname=models.CharField(max_length=100)
-#     sname=models.CharField(max_length=100)
-#     gender=models.CharField(choices=[('MALE','male'),
-#                      ('FEMALE','female'),
-#                      ('OTHER','other')])
-#     dob=models.DateField()
-#     email=models.EmailField(unique=True)
-#     contact_number=models.CharField()
-#     age=models.IntegerField()
-#     # hobbies = models.ManyToManyField()
-#     # country,state,city Dropdown !!!
-#     # avatar = models.ImageField(upload_to="avatars/")
+    fname=models.CharField(max_length=100)
+    sname=models.CharField(max_length=100)
+    gender=models.CharField(choices=[('MALE','male'),
+                     ('FEMALE','female'),
+                     ('OTHER','other')])
+    dob=models.DateField()
+    email=models.EmailField(unique=True)
+    contact_number=models.CharField()
+    age=models.IntegerField()
+    hobbies = models.JSONField(default=list)
+    # country,state,city Dropdown !!!
+    image=models.ImageField(upload_to="documents/")
 
     
 
