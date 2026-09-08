845c5d87bf476b6af552cf7e32369d932bd2084a
anandsuresh17 <anandsuresh1704@gmail.com>
2026-09-08 16:02:43 +0530

    updation in the structure of registration form

--- apps1/models.py
+++ apps1/models.py
@@ -1,25 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-class Hobby(models.Model):
-    name = models.CharField(max_length=100)
 
 class Students(models.Model):
 
-# columns-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    fname=models.CharField(max_length=100)
-    sname=models.CharField(max_length=100)
-    gender=models.CharField(choices=[('MALE','male'),
-                     ('FEMALE','female'),
-                     ('OTHER','other')])
-    dob=models.DateField()
-    email=models.EmailField(unique=True)
-    contact_number=models.CharField()
-    age=models.IntegerField()
+    fname = models.CharField(max_length=100)
+    sname = models.CharField(max_length=100)
+
+    gender = models.CharField(max_length=10, choices=[
+        ('MALE', 'male'),
+        ('FEMALE', 'female'),
+        ('OTHER', 'other')
+    ])
+
+    dob = models.DateField()
+
+    contact_number = models.CharField(max_length=15)
+
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
     hobbies = models.JSONField(default=list)
-    # country,state,city Dropdown !!!
-    image=models.ImageField(upload_to="documents/")
 
-    
+    image = models.ImageField(
+        upload_to="documents/",
+        blank=True,
+        null=True
+    )
 
+    first_login = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.fname + " " + self.sname
--- myproject/settings.py
+++ myproject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/"static"]
-
-LOGIN_URL="login/"
-LOGIN_REDIRECT_URL="dashboard/"  
-LOGOUT_REDIRECT_URL="login/"  
+ 
+# LOGIN_URL="login/"
+# LOGIN_REDIRECT_URL="dashboard/"  
+# LOGOUT_REDIRECT_URL="login/"  
