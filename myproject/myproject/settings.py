e24f886f0fa466c64a84d9b684b7f79c4acb39aa
anandsuresh17 <anandsuresh1704@gmail.com>
2026-09-16 21:54:44 +0530

    Move secrets to environment variables

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wtda%x!ch+x9=&!3g8g3n4#%asy@&n@l%mlh3c@w=jpcz#rhnf'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- myproject/settings.py
+++ myproject/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '[REDACTED]'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -132,4 +131,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER='eduhubarg72@gmail.com'
-EMAIL_HOST_PASSWORD='***REMOVED***'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
